1274d83473dce39b771f3ee3fd27f20a7e3de322
FinemechanicPub <93194456+FinemechanicPub@users.noreply.github.com>
2025-08-15 03:10:17 +0300

    Add tests for list api views

---
+++ api/tests/conftest.py
@@ -0,0 +1,46 @@
+import random
+import pytest
+
+from albums.models import Album, Artist, Track
+
+
+@pytest.fixture
+def tracks():
+    TRACK_TITLE = "Track {n}"
+    return [
+        Track.objects.create(title=TRACK_TITLE.format(n=i))
+        for i in range(5)
+    ]
+
+
+@pytest.fixture
+def artists():
+    ARTIST_NAME = "Artist {n}"
+    return [
+        Artist.objects.create(name=ARTIST_NAME.format(n=i))
+        for i in range(5)
+    ]
+
+
+@pytest.fixture
+def albums(artists, tracks):
+    ALBUM_TITLE = "Album {n}"
+    artist = artists[-1]
+    albums = [
+        Album.objects.create(
+            title=ALBUM_TITLE.format(n=i),
+            artist=artist,
+            year=random.randint(1980, 2025)
+        )
+        for i in range(3)
+    ]
+
+    def add_tracks(album: Album, tracks: list[Track]):
+        for i, track in enumerate(tracks, start=1):
+            album.tracks.add(track, through_defaults={"position": i})
+
+    n = len(tracks)
+    add_tracks(albums[0], tracks[:n // 2])
+    add_tracks(albums[1], tracks[n // 2:])
+    add_tracks(albums[2], tracks)
+    return albums
---
+++ api/tests/test_views.py
@@ -0,0 +1,86 @@
+from django.test import Client
+from django.urls import reverse
+import pytest
+
+pytestmark = pytest.mark.django_db
+
+
+ERROR_STATUS = (
+    "{method} request on {url} returned status {status}, "
+    "while expected {expected}"
+)
+
+ERROR_COUNT = (
+    "{method} request on {url} returned {actual} objects, "
+    "while expected {expected}"
+)
+ERROR_NO_OBJECT = (
+    "Object with id {id} not found in response to {method} request to {url}"
+)
+
+ERROR_OBJECT_FIELD = (
+    "{method} request on {url} returned object {obj} with attribute"
+    "atrr={attr}, while expected {expected}"
+)
+
+
+def check_enity(url, obj, data: dict, fields: list[str]):
+    for field in fields:
+        assert str(getattr(obj, field)) == str(data[field]), (
+            ERROR_OBJECT_FIELD.format(
+                method="GET",
+                url=url,
+                obj=obj,
+                attr=field,
+                expected=str(getattr(obj, field))
+            )
+        )
+
+
+def group_by_id(results: list[dict[str, str]]) -> dict[str, dict[str, str]]:
+    return {
+        result["id"]: {
+            field: value for field, value in result.items() if field != "id"
+            }
+        for result in results
+    }
+
+
+def check_list_api(client: Client, objs, view: str, fields: list):
+    url = reverse(view)
+    response = client.get(url)
+    status = response.status_code
+    assert status == 200, (
+        ERROR_STATUS.format(method="GET", url=url, status=status, expected=200)
+    )
+    count = response.data["count"]
+    assert (
+        count == len(objs)
+    ), ERROR_COUNT.format(method="GET", url=url, actual=len(objs))
+    data = group_by_id(response.data["results"])
+    for obj in objs:
+        assert str(obj.pk) in data, ERROR_NO_OBJECT.format(
+            id=str(obj.pk), method="GET", url=url
+        )
+        check_enity(url, obj, data[str(obj.pk)], fields)
+
+
+def test_track_list(client, tracks):
+    check_list_api(
+        client, objs=tracks, view="api:track-list", fields=["title"]
+    )
+
+
+def test_artist_list(client, artists):
+    check_list_api(
+        client, objs=artists, view="api:artist-list", fields=["name"]
+    )
+
+
+def test_album_list(client, albums):
+    check_list_api(
+        client,
+        objs=albums,
+        view="api:album-list",
+        fields=["title", "year", "artist"]
+    )
