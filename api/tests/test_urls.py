906be55726f9644fe76d68066e744fc2b0ca36a2
FinemechanicPub <93194456+FinemechanicPub@users.noreply.github.com>
2025-08-16 03:37:44 +0300

    Formatting

--- api/tests/test_urls.py
+++ api/tests/test_urls.py
@@ -4,18 +4,16 @@ import pytest
 
 pytestmark = pytest.mark.django_db
 
-WRONG_STATUS = (
-    "A {method} request to {url} returned {status}, expected {expected}"
-)
+WRONG_STATUS = "A {method} request to {url} returned {status}, expected {expected}"
 
 
 @pytest.mark.parametrize(
-        ["url", "expected"],
-        [
-            (reverse("api:track-list"), HTTPStatus.OK),
-            (reverse("api:artist-list"), HTTPStatus.OK),
-            (reverse("api:album-list"), HTTPStatus.OK),
-        ]
+    ["url", "expected"],
+    [
+        (reverse("api:track-list"), HTTPStatus.OK),
+        (reverse("api:artist-list"), HTTPStatus.OK),
+        (reverse("api:album-list"), HTTPStatus.OK),
+    ],
 )
 def test_get_response(client, url, expected):
     status = client.get(url).status_code
